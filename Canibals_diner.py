272f1b89ef54feb754ada686921c7166900fb84d
Victor Ribeiro <44476242+jvictorribeiro@users.noreply.github.com>
2019-04-16 21:50:29 -0300

    Update Canibals_diner.py

--- Canibals_diner.py
+++ Canibals_diner.py
@@ -21,23 +21,23 @@ class Canibal(object):
         self.name = name
         workingThread = Thread()
         self.run()
-        self.porcao = 0
         workingThread.start()
+        #porcao = 0
          
     def run(self):
-        while True:
-        	self.serving()
+        #while True:
+            self.serving()
         
     def serving(self):
         global caldron
         
         mutex.acquire()
         if caldron == 0:
-        	mutex.release()
-        	logging.debug('Waking up cooker')
-        	CookerEvent.set()   
-        	CanibalEvent.clear()
-        	CanibalEvent.wait()
+            mutex.release()
+            logging.debug('Waking up cooker')
+            CookerEvent.set()   
+            CanibalEvent.clear()
+            CanibalEvent.wait()
         else:
             caldron -= 1
             logging.debug('{} is serving the food'.format(self.name))
@@ -46,49 +46,45 @@ class Canibal(object):
             self.eating()
 
     def eating(self):
-        self.porcao += 1
         print('{} is eating'.format(self.name))
         time.sleep(canibalEating)
+        #self.porcao += 1
 
 class Cooker(object):
-
     def __init__(self):
-    	workingThread = Thread()
-    	self.run()
-    	workingThread.start()
+        workingThread = Thread()
+        self.run()
+        workingThread.start()
     
     def run(self):
-    	self.cooking()
+        self.cooking()
         
     def cooking(self):
-    	global CookerEvent
-		global CanibalEvent
-	
-			logging.debug('The diner is being cooked')
-   		mutex.acquire()
+        global CookerEvent
+        global CanibalEvent
 
-    	caldron = 5
-    	mutex.release()
-    	time.sleep(cookerCooking)
+        logging.debug('The diner is being cooked')
+        mutex.acquire()
 
-    	logging.debug('the diner is ready')
-    	logging.debug('waking canibals')
+        caldron = 5
+        mutex.release()
+        time.sleep(cookerCooking)
 
-    	CanibalEvent.set()
+        logging.debug('the diner is ready')
+        logging.debug('waking canibals')
 
-    	CookerEvent.clear() 
-    	CookerEvent.wait()
+        CanibalEvent.set()
+        CookerEvent.clear()
+        CookerEvent.wait()
 
   
 if __name__ == '__main__':
     
     canibals = []
     canibals.append(Canibal('Canibal1'))
-        #porcoes1+=1
     canibals.append(Canibal('Canibal2'))
-        #porcoes2+=1
     canibals.append(Canibal('Canibal3'))
-        #porcoes3+=1
+
     cooker = Cooker()
 
     for canibal in canibals:
